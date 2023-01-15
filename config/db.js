09e7d76f90b654657eb48381a2417abb38a0d597
Harsh Gawli <harshgawli28@gmail.com>
2023-01-15 11:34:54 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  } catch (err) {
+    console.log(`Error: ${err.message}`.red);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
